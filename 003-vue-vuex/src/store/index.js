f1e1147975a2906c6108124b5851743a5987fead
renzhongrui <zrzoffice@163.com>
2020-02-24 00:00:32 +0800

    vuex的基本使用

--- 003-vue-vuex/src/store/index.js
+++ 003-vue-vuex/src/store/index.js
@@ -4,11 +4,26 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  strict: true,
+  getters: { // 类似于vue中computer计算属性
+      myName(state) {
+        return state.name + "-007";
+      }
   },
-  mutations: {
+  state: { // 相当于vue中的data
+    name : "tom"
   },
-  actions: {
+  mutations: { // 同步方法,通过mutations更改数据只能通过同步方法(严格模式下使用)，通过commit来调用
+   SET_NAME:(state,value) =>{
+     state.name = value;
+    }
+  },
+  actions: { // 通过异步方法更改数据
+    changeName({commit}, value) { // 通过dispatch来调用
+      setTimeout(()=> {
+        commit("SET_NAME",value);
+      },1000);
+    }
   },
   modules: {
   }
--- 003-vue-vuex/src/views/Home.vue
+++ 003-vue-vuex/src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>真实名字是：{{$store.state.name}}</p>
+    <p>特工名字是：{{$store.getters.myName}}</p>
+    <p>mapGetters是：{{myName}}</p>
+    <button @click="syncCommit()">同步点击改变</button>
+    <button @click="asyncDispatch()">异步点击改变</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// 引入mapGetters，并在computed中使用
+import {mapGetters} from 'vuex'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  computed: {
+    ...mapGetters([
+      'myName'
+    ])
+  },
+  methods: {
+    syncCommit() { // 同步调用store的mutations中的方法
+      this.$store.commit("SET_NAME","张三");
+    },
+    asyncDispatch() {
+      this.$store.dispatch("changeName","李四");
+    }
   }
+
 }
 </script>
